affaa32ee42c3834915e885dfceae62604d3d5c3
896299533 <896299533@qq.com>
2020-11-04 19:04:02 +0800

    行内，块级元素

---
+++ 第十三次课/js初步学习.js
@@ -0,0 +1,10 @@
+
+
+function orange(){
+    var array = [123,23,45,67,18,59,1,41]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+orange();
